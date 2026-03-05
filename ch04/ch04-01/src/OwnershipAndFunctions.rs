7dca09bc7f99a188899d2e1703dda57e2a2105d3
Rogério Ramos Maldonado <rogerioramosm@gmail.com>
2026-03-05 16:15:46 -0400

    4.1 - finalização de capitulo

---
+++ ch04/ch04-01/src/OwnershipAndFunctions.rs
@@ -0,0 +1,53 @@
+fn main() {
+    // {
+    //   let s = "hello";
+
+    //   println!("{s}");
+    // }
+
+    // let mut s = String::from("hello");
+    // s.push_str(", world!");
+
+    // println!("{s}");
+
+    // let x = 5;
+    // let y = x;
+
+    // println!("{} {}", x, y);
+
+    // let s1 = String::from("hello");
+    // let s2 = s1;
+
+    // println!("{} {}", s1, s2);
+
+    // let mut s = String::from("hello");
+    // s = String::from("ahoy");
+
+    // println!("{s} world!");
+
+    /*
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s1 = {s1}, s2 = {s2}");
+
+    let x = 5;
+    let y = x;
+
+    println!("x = {x}, y = {y}");
+    */
+
+    let s = String::from("hello");
+    takes_ownership(s);
+
+    let x = 5;
+    makes_copy(x);
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{some_string}");
+}
+
+fn makes_copy(some_string: i32) {
+    println!("{some_string}");
+}
--- ch04/ch04-01/src/main.rs
+++ ch04/ch04-01/src/main.rs
@@ -1,37 +1,13 @@
 fn main() {
-    // {
-    //   let s = "hello";
-
-    //   println!("{s}");
-    // }
-
-    // let mut s = String::from("hello");
-    // s.push_str(", world!");
-
-    // println!("{s}");
-
-    // let x = 5;
-    // let y = x;
-
-    // println!("{} {}", x, y);
-
-    // let s1 = String::from("hello");
-    // let s2 = s1;
-
-    // println!("{} {}", s1, s2);
-
-    // let mut s = String::from("hello");
-    // s = String::from("ahoy");
-
-    // println!("{s} world!");
-
     let s1 = String::from("hello");
-    let s2 = s1.clone();
 
-    println!("s1 = {s1}, s2 = {s2}");
+    let (s2, len) = calculate_length(s1);
 
-    let x = 5;
-    let y = x;
+    println!("The length os '{s2}' is {len}.");
+}
 
-    println!("x = {x}, y = {y}");
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); //len() retorna o tamanho da string
+
+    (s, length)
 }
